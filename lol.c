11403f78df5c98945336bb7e2f1d7d94f8ecd57a
ronritari <ron.ritari@gmail.com>
2022-01-10 11:13:32 +0200

    itdo

--- lol.c
+++ lol.c
@@ -1,19 +1,69 @@
 #include <stdio.h>
 int main(void)
-{       
-  int tahti;
-  scanf("%d",&tahti);
-  int rivi=tahti;
-  for(int k=0;k<tahti;k++){
-    for(int i=0;i<rivi;i++){
+{
+  printf("Input rows: \n");
+  int out, in;
+  int row_size;
+  scanf("%d", &row_size);
+  int print_control_x = 1;
+  int print_control_y = row_size * 2 - 1;
+  int x=row_size;
+  int y=row_size;
+  
+  for (out = row_size; out >= 1; out--)
+  {
+    printf(" ");
+    for (in = 1; in <= row_size * 2; in++)
+    {       
+      
+      if (out==1){
+        
+      }
+      else if (in == print_control_x || in == print_control_y)
+      {
+        printf("%d", print_control_x);
+      }
+      else
+      {
+        printf(" ");
+      }
+    }
+    print_control_x++;
+    print_control_y--;
+    if(out==1){
+
+    }else{
+      printf("\n");
+    }
+    
+  }
+  for (out = 1; out <= row_size; out++)
+  {
+    if(out==1){
+
+    }else{
       printf(" ");
     }
-    for(int t=0;t<tahti;t++){
-    printf("*");
-    }         
+    
+    for (in = 1; in <= row_size * 2; in++)
+    {
+      if (in == x|| in == y)
+      {
+        printf("%d",x);
+      }
+      else
+      {
+        printf(" ");
+      }
+    }
+    x--;
+    y++;
     printf("\n");
-    rivi--;
   }
+
+
 }
+
+
 
 
