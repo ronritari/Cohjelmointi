29280f5a000d669dfc4bd77f58c004ecbf905316
ronritari <83445287+ronritari@users.noreply.github.com>
2022-03-29 13:07:01 +0300

    ok

---
+++ sin.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+#include <ncurses.h>
+
+void draw_axis(void);
+int scale_sin(float value);
+
+int main(){
+
+
+}
+
+void draw_axis(void){
+
+}
+
+int scale_sin(float value){
+
+}
